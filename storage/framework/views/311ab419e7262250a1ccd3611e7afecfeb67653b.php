3e3ef53f8bfef47d06662d15adba7704ace537d8
adittiyaMaulana <66076612+adittiyaMaulana@users.noreply.github.com>
2020-10-15 21:42:25 +0700

    routes storages and test

---
+++ storage/framework/views/311ab419e7262250a1ccd3611e7afecfeb67653b.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <title>My Indonesia Homepage</title>
+</head>
+<body style="font-family: Verdana;"> 
+
+  <article  style="background-color: black;">
+    <header>
+      <div style="color: white; text-align: center; font-size: 25px;">
+        KMG My Indonesia
+      </div>
+      
+      <div style="color: white; text-align: center; font-size: 12px;">
+        Enjoy The Beauty of Indonesia
+      </div>
+      
+      <br>
+
+      <ul class="nav justify-content-center">
+        <li class="nav-item">
+          <a style="font-size: 12px; color: white;" class="nav-link active" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a style="font-size: 12px; color: white;" class="nav-link" href="#">Event</a>
+        </li>
+        <li class="nav-item">
+          <a style="font-size: 12px; color: white;" class="nav-link" href="#">Travel</a>
+        </li>
+        <li class="nav-item">
+          <a style="font-size: 12px; color: white;" class="nav-link" href="#">Order</a>
+        </li>
+        <li class="nav-item">
+          <a style="font-size: 12px; color: white;" class="nav-link" href="#">About</a>
+        </li>
+      </ul>
+    </header>
+  </article>
+
+
+  <article style="background-color: black;">
+  <div class="container">
+  <div class="row">
+    <div class="col" style="text-align: center;">
+      <img src="Views/Assets/dance.jpg" alt="dance">
+    </div>
+    <div class="col" style="text-align: center;">
+      2 of 2
+    </div>
+  </div>
+  </article>
+
+<!-- <div class="tab-content" id="myTabContent">
+  <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">...</div>
+  <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">...</div>
+  <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">...</div>
+</div> -->
+
+</body>
+</html><?php /**PATH C:\xampp\htdocs\KMG_My_Indonesia\resources\views/homepage.blade.php ENDPATH**/ ?>
