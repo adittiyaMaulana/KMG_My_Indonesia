d8758d0856508ac1c1fae8f763bd29bc9f1c4f04
adittiyaMaulana <66076612+adittiyaMaulana@users.noreply.github.com>
2020-10-16 23:46:53 +0700

    commit 2
    Kurang responsive pada bagian image, tolong perbaiki

---
+++ homepage.blade.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <title>My Indonesia Homepage</title>
+</head>
+<body style="font-family: Verdana;"> 
+@csrf
+  <article  style="background-color: black;">
+    <header>
+      <div style="color: white; text-align: center; font-size: 25px;">
+        KMG My Indonesia
+      </div>
+      
+      <div style="color: white; text-align: center; font-size: 12px;">
+        Enjoy The Beauty of Indonesia
+      </div>
+      
+    <div style="color: gray; text-align: center;">
+    --------------------------------------------------
+    </div>
+
+      <ul class="nav justify-content-center">
+        <li class="nav-item">
+          <a style="font-size: 12px; color: white;" class="nav-link active" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a style="font-size: 12px; color: white;" class="nav-link" href="#">Event</a>
+        </li>
+        <li class="nav-item">
+          <a style="font-size: 12px; color: white;" class="nav-link" href="#">Travel</a>
+        </li>
+        <li class="nav-item">
+          <a style="font-size: 12px; color: white;" class="nav-link" href="#">Order</a>
+        </li>
+        <li class="nav-item">
+          <a style="font-size: 12px; color: white;" class="nav-link" href="#">About</a>
+        </li>
+      </ul>
+    </header>
+
+  </article>
+  
+  <article style="background-color: black; border: 20px solid gray; border-right: 90px solid gray;
+  border-left: 90px solid gray;">
+  <div class="container">
+  <div class="row">
+    <div class="col" style="text-align: left;">
+    <img src="/images/dance.jpg" alt="dance" style="width: 100%; margin-left: -16px; position: relative; display: block;" />
+    </div>
+    <div class="col" style="text-align: center; margin-top: 90px;
+    font-size: 35px; color: white;">
+      <div>KMG</div>
+      <div>My Indonesia</div>
+    </div>
+  </div>
+  </article>
+  
+  <article style="margin-top: 50px;">
+  <div style="justify-content: center; text-align: center;">
+    All Post
+  </div>
+  <div class="row mb-2" style="justify-content: center; margin-top: 20px;">
+    <div class="col-md-6">
+      <div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+        <div class="col p-4 d-flex flex-column position-static">
+          <strong class="d-inline-block mb-2 text-primary" style="font-size: 10px;">
+            Adit Tiya Maulana
+          </strong>
+          <h6 class="mb-0">Dita Miranda Jasjfi Memeriahkan Festival Budaya Di Jawa Timur</h6>
+          <div class="mb-1 text-muted" style="font-size: 8px; margin-top: 10px;">Nov 12</div>
+          <p class="card-text mb-auto" style="font-size: 10px;">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
+          <a href="#" class="stretched-link" style="font-size: 10px;">Continue reading</a>
+        </div>
+        <div class="col-auto d-none d-lg-block">
+          <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+        </div>
+      </div>
+    </div>
+  </article>
+
+  <article>
+  <div class="row mb-2" style="justify-content: center; margin-top: 20px;">
+    <div class="col-md-6">
+      <div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+        <div class="col p-4 d-flex flex-column position-static">
+          <strong class="d-inline-block mb-2 text-primary" style="font-size: 10px;">
+            Adit Tiya Maulana
+          </strong>
+          <h6 class="mb-0">Gamelan Menarik Perhatian Warga Amerika</h6>
+          <div class="mb-1 text-muted" style="font-size: 8px; margin-top: 10px;">Nov 12</div>
+          <p class="card-text mb-auto" style="font-size: 10px;">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
+          <a href="#" class="stretched-link" style="font-size: 10px;">Continue reading</a>
+        </div>
+        <div class="col-auto d-none d-lg-block">
+          <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+        </div>
+      </div>
+    </div>
+  </article>
+<!-- <div class="tab-content" id="myTabContent">
+  <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">...</div>
+  <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">...</div>
+  <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">...</div>
+</div> -->
+
+</body>
+</html>
